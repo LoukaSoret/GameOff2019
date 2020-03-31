8104f42cce3413aaf57f2e996a085cbbf1bf7704
Louka Soret <louka.soret@gmail.com>
2020-03-31 20:11:58 +0200

    fix shadercache

--- Scripts/ShaderCache.gd
+++ Scripts/ShaderCache.gd
@@ -7,7 +7,11 @@ var count_down = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$CristalParticules1.emitting = true
+	$CristalParticules2.emitting = true
+	$DustCircle.emitting = true
+	$Hit.emitting = true
+	$DustTrail.emitting = true
 
 func _process(delta: float) -> void:
 	if count_down > 0:
--- Scripts/global.gd
+++ Scripts/global.gd
@@ -3,14 +3,13 @@ extends Node
 onready var root = get_tree().get_root()
 onready var loading_progress = root.find_node("LoadingProgress",true,false)
 
-var full_screen = false
 var load_thread : Thread = Thread.new()
 var loader : ResourceInteractiveLoader
 
 func _ready():
 	#get_viewport().set_size_override(true,Vector2(1920,1080))
 	#OS.set_window_size(Vector2(1280,720))
-	#OS.window_fullscreen = false
+	OS.window_fullscreen = false
 	set_process(false)
 
 func update_progress():
@@ -40,13 +39,9 @@ func load_thread_func(userdata):
 	loader = null
 	get_tree().change_scene_to(resource)
 
-"""
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
-		if global.full_screen == false:
+		if not OS.window_fullscreen:
 			OS.window_fullscreen = true
-			global.full_screen = true
 		else :
 			OS.window_fullscreen = false
-			global.full_screen = false
-"""
